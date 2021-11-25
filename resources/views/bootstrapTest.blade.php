3939d457037b5dbc8b9ee9d9b8b1db31f3a47c40
Daniel Alexander Kefas <6181801001@student.unpar.ac.id>
2021-11-25 10:22:36 +0700

    Adding Bootstrap and Controller

---
+++ resources/views/bootstrapTest.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Repodok') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="https://use.fontawesome.com/afc115f24d.js"></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Fjalla+One&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    <link rel="icon" href="{{ url('css/favicon.png') }}">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <h1>Hello World!</h1>
+</body>
+</html>
