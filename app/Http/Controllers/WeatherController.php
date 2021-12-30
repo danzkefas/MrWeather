0ea27d096303b3515f09554b2a727cb3606c8bc4
Daniel Alexander Kefas <6181801001@student.unpar.ac.id>
2021-12-30 12:50:29 +0700

    Added Session for Data Insight

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Session;
 
 class WeatherController extends Controller
 {
@@ -30,6 +31,23 @@ class WeatherController extends Controller
             //One Call API
             $responseFuture = Http::get("https://api.openweathermap.org/data/2.5/onecall?lat={$lat}&lon={$lon}&exclude=current,minutely,hourly,alerts&appid={$apiKey}&units=metric");
             $responseFutureJSON = $responseFuture->json();
+
+            //Set Session for Insight
+            if(!Session::has('hotTemp') || !Session::has('coldTemp')){
+                Session::put('hotTemp', [ $responseJSON['name'], $responseJSON['main']['temp'] ]); 
+                Session::put('coldTemp', [ $responseJSON['name'], $responseJSON['main']['temp'] ]); 
+            } else {
+                $lastHotTemp = Session::get('hotTemp')[1];
+                $lastColdTemp = Session::get('coldTemp')[1];
+                if( $responseJSON['main']['temp'] > $lastHotTemp ){
+                    Session::put('hotTemp', [ $responseJSON['name'], $responseJSON['main']['temp'] ]);
+                }
+
+                if( $responseJSON['main']['temp'] < $lastColdTemp ){
+                    Session::put('coldTemp', [ $responseJSON['name'], $responseJSON['main']['temp'] ]);
+                }
+                
+            }
             return view('index', compact('responseJSON', 'responseFutureJSON'));
         } else {
             $responseCode = $response->getStatusCode();
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -26,65 +26,74 @@
 
 <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column mb-5">
     <main class="px-3">
+        <div class="alert alert-info alert-dismissible fade show" role="alert">
+            
+            <h4 class="alert-heading">Data Insight</h4>
+            <div>
+                Berdasarkan pencarian Anda: <br>
+                Kota Terpanas: {{ Session::get('hotTemp')[0] }} (<span class="text-danger">{{ Session::get('hotTemp')[1] }}&#176;C</span>) <br>
+                Kota Terdingin:  {{ Session::get('coldTemp')[0] }} (<span class="text-primary">{{ Session::get('coldTemp')[1] }}&#176;C</span>)
+            </div>
+            <button type="button" class="btn-close mx-auto" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
         @isset($responseJSON)
-            <div class="container text-white rounded-top main-weather">
-                <div class="row p-3 d-flex align-items-center">
-                    <div class="col col-sm-3 text-center">
-                        <div class="row">
-                            <h1 class="m-0 fw-bold">{{ round($responseJSON['main']['temp']) }}&#176;C</h1>
-                        </div>
-                        <div class="row">
-                            <h6 class="m-0">Feels like {{ round($responseJSON['main']['feels_like']) }}&#176;C<h6>
-                        </div>
+        <div class="container text-white rounded-top main-weather">
+            <div class="row p-3 d-flex align-items-center">
+                <div class="col col-sm-3 text-center">
+                    <div class="row">
+                        <h1 class="m-0 fw-bold">{{ round($responseJSON['main']['temp']) }}&#176;C</h1>
                     </div>
-                    <div class="col col-sm-5">
-                        <div class="row">
-                            <span class="fw-bold p-0 fs-5">{{ ucfirst($responseJSON['weather'][0]['description']) }}</span>
-                        </div>
-                        <div class="row">
-                            <span class="fs-6 p-0">{{ $responseJSON['name'] }}, {{ $responseJSON['sys']['country'] }}</span>
-                        </div>
-                    </div>
-                    <div class="col col-sm-4 p-0 text-end">
-                        <img src="http://openweathermap.org/img/wn/{{ $responseJSON['weather'][0]['icon'] }}@4x.png" alt="weather_icon">
+                    <div class="row">
+                        <h6 class="m-0">Feels like {{ round($responseJSON['main']['feels_like']) }}&#176;C<h6>
                     </div>
                 </div>
-            </div>
-            <!-- Looping Side -->
-            <div class="container text-white rounded-bottom loop">
-                <!-- Loop Here -->
-                @for ($i = 1; $i <= 5; $i++)
-                <div class="row p-1 d-flex align-items-center">
-                    <div class="col col-sm-2 text-center">
-                        {{ strtoupper(\Carbon\Carbon::createFromTimestamp($responseFutureJSON['daily'][$i]['dt'])->format('D')) }}
+                <div class="col col-sm-5">
+                    <div class="row">
+                        <span class="fw-bold p-0 fs-5">{{ ucfirst($responseJSON['weather'][0]['description']) }}</span>
                     </div>
-                    <div class="col col-sm-7">
-                        <img src="http://openweathermap.org/img/wn/{{ $responseFutureJSON['daily'][$i]['weather'][0]['icon'] }}.png" alt="weather_icon"> {{ ucfirst($responseFutureJSON['daily'][$i]['weather'][0]['description']) }}
-                    </div>
-                    <div class="col col-sm-3 text-end">
-                        <div class="row">
-                            Max: {{ round($responseFutureJSON['daily'][$i]['temp']['max']) }}&#176;C
-                        </div>
-                        <div class="row">
-                            Min: {{ round($responseFutureJSON['daily'][$i]['temp']['min']) }}&#176;C
-                        </div>
+                    <div class="row">
+                        <span class="fs-6 p-0">{{ $responseJSON['name'] }}, {{ $responseJSON['sys']['country'] }}</span>
                     </div>
                 </div>
-                @endfor
-            </div>
-        @endisset
-        @empty($responseJSON)
-            <div class="container text-white rounded-3 main-weather">
-                <div class="row p-2 d-flex text-center">
-                    <h1>{{ $responseCode }} | {{ $responsePhr }}</h1>
+                <div class="col col-sm-4 p-0 text-end">
+                    <img src="http://openweathermap.org/img/wn/{{ $responseJSON['weather'][0]['icon'] }}@4x.png" alt="weather_icon">
                 </div>
             </div>
-        @endempty
-    </main>
+        </div>
+        <!-- Looping Side -->
+        <div class="container text-white rounded-bottom loop">
+            <!-- Loop Here -->
+            @for ($i = 1; $i <= 5; $i++) <div class="row p-1 d-flex align-items-center">
+                <div class="col col-sm-2 text-center">
+                    {{ strtoupper(\Carbon\Carbon::createFromTimestamp($responseFutureJSON['daily'][$i]['dt'])->format('D')) }}
+                </div>
+                <div class="col col-sm-7">
+                    <img src="http://openweathermap.org/img/wn/{{ $responseFutureJSON['daily'][$i]['weather'][0]['icon'] }}.png" alt="weather_icon"> {{ ucfirst($responseFutureJSON['daily'][$i]['weather'][0]['description']) }}
+                </div>
+                <div class="col col-sm-3 text-end">
+                    <div class="row">
+                        Max: {{ round($responseFutureJSON['daily'][$i]['temp']['max']) }}&#176;C
+                    </div>
+                    <div class="row">
+                        Min: {{ round($responseFutureJSON['daily'][$i]['temp']['min']) }}&#176;C
+                    </div>
+                </div>
+        </div>
+        @endfor
+</div>
+@endisset
+@empty($responseJSON)
+<div class="container text-white rounded-3 main-weather">
+    <div class="row p-2 d-flex text-center">
+        <h1>{{ $responseCode }} | {{ $responsePhr }}</h1>
+    </div>
+</div>
+@endempty
+</main>
 
-    <footer class="footer fixed-bottom text-white-50 text-center py-3 bg-dark">
-        <span>Powered by OpenWeatherMap API</span>
-    </footer>
+<footer class="footer fixed-bottom text-white-50 text-center py-3 bg-dark">
+    <span>Powered by OpenWeatherMap API</span>
+</footer>
 
 </div>
 
